1c802c09ce74578f826fa7ffa96534a0b8fd7bb8
annakrystalli <annakrystalli@googlemail.com>
2018-04-29 22:52:49 +0100

    separate update_meta_tbl documentation

--- R/utils.R
+++ R/utils.R
@@ -1,13 +1,13 @@
 
-#' Create attribute shell
+#' Create metadata shell
 #'
-#' Create shell metadata table of variable attributes for a dataset. The function attempts to guess
-#' guess the values where possible. The rest will need completing manually.
+#' Create metadata table shell of variable attributes from a dataset. The function attempts to
+#' guess values where possible. The rest require completing manually
 #' @param df a dataframe of data for which metadata attribute table is to be created
 #' @param factor_cols character string or vector of names of columns containing factors
+#' Code and level information is extracted auromatically from factor columns.
 #' @param sep separator used to separate codes and definitions for each level in
 #'  the metadata table
-#' @param meta_tbl a metadata table associated with df
 #'
 #' @return a dataframe consiting of one row per column of the input df. Columns
 #'  represent minimum metadata requirements.
@@ -53,11 +53,25 @@ create_meta_shell <- function(df, factor_cols = NULL, sep = ";"){
         }
     }
 
-    return(meta)
+    tibble::as.tibble(meta)
 }
 
+#' Update metadata table
+#'
+#' Update the metadata table of a dataset. The function will identify variable columns in df
+#' for which rows are missing in meta_tbl and append new rows for each. It will also
+#' remove any deprecated rows which do not match columns in the dataset.
+#' @param df a dataframe or tibble to which the metadata table relates to
+#' @param meta_tbl a metadata table associated with df
+#' @param factor_cols character string or vector of names of columns containing
+#' factors (will only apply to new columns)
+#' @param sep separator used to separate codes and definitions for each level in
+#'  the metadata table (must be the same used throughout meta_tbl)
+#'
+#' @return an updated meta_tbl with rows added for new df columns and any
+#' deprecated rows trimmed.
+#'
 #' @export
-#' @rdname create_meta_shell
 update_meta_tbl <- function(df, meta_tbl, factor_cols = NULL, sep = ";") {
     if(all(names(df) %in% meta_tbl$attributeName) &
        all(meta_tbl$attributeName %in% names(df))){return(meta_tbl)}
@@ -72,7 +86,7 @@ update_meta_tbl <- function(df, meta_tbl, factor_cols = NULL, sep = ";") {
     meta_tbl <- meta_tbl[match(names(df), meta_tbl$attributeName),]
     row.names(meta_tbl) <- NULL
 
-    meta_tbl
+    tibble::as.tibble(meta_tbl)
 }
 
 #' Extract attribute table
@@ -89,7 +103,7 @@ extract_attr_tbl <- function(meta_tbl) {
                  "unit", "minimum", "maximum", "formatString", "definition")
     out <- meta_tbl[,attr_hd]
     out$columnClasses <- NA
-    out
+    tibble::as.tibble(out)
 }
 
 #' Extract factors table
@@ -116,7 +130,7 @@ extract_factors_tbl <- function(meta_tbl, sep =";") {
         )
         factors <- rbind(factors, f_tbl)
     }
-    return(factors)
+    tibble::as.tibble(factors)
 }
 
 # extract factor data from a vector and return a collapsed string
