ce82eeb293087701ad85776cccea87acf0601bf7
Anna Krystalli <annakrystalli@googlemail.com>
2019-03-26 12:44:13 +0000

    add var and factor functionality

--- R/utils.R
+++ R/utils.R
@@ -37,21 +37,11 @@ mt_create_meta_shell <- function(df, factor_cols = NULL, sep = ";"){
     meta$columnClasses <- sapply(df, class)
     meta$columnClasses[meta$columnClasses == "integer"] <- "numeric"
 
-    if(any(sapply(df, FUN = is.factor))){
-        factor_cols <- unique(c(factor_cols,
-                                names(df)[sapply(df, FUN = is.factor)]))
+    if(!is.null(factor_cols)){
+        meta_tbl <- mt_update_factors(df, meta_tbl = meta,
+                                      factor_cols = factor_cols)
     }
 
-    if(!is.null(factor_cols)){
-        if(!all(factor_cols %in% names(df))){
-            stop(factor_cols[which(!factor_cols %in% names(df))],
-                 " not a valid colname")
-        }
-        for(col in factor_cols){
-            meta[meta$attributeName == col,c("code", "levels")] <-
-                collapse_factor_levels(df[,col, drop = T], sep)
-        }
-    }
 
     tibble::as.tibble(meta)
 }
@@ -71,6 +61,7 @@ mt_create_meta_shell <- function(df, factor_cols = NULL, sep = ";"){
 #' @return an updated meta_tbl with rows added for new df columns and any
 #' deprecated rows trimmed.
 #'
+#' @family \code(meta_tbl) utilities
 #' @export
 mt_update_meta_tbl <- function(df, meta_tbl, factor_cols = NULL, sep = ";") {
     if(all(names(df) %in% meta_tbl$attributeName) &
@@ -87,6 +78,51 @@ mt_update_meta_tbl <- function(df, meta_tbl, factor_cols = NULL, sep = ";") {
     row.names(meta_tbl) <- NULL
 
     tibble::as.tibble(meta_tbl)
+}
+
+
+
+
+#' @inherit mt_update_meta_tbl
+#' @export
+#' @examples
+#' \dontrun{
+#' library(gapminder)
+#' df <- gapminder::gapminder
+#' meta_tbl <- readr::read_csv(file.path(here::here("inst", "extdata",
+#'                                                  "gapminder_meta.csv")))
+#' # update metadata for factor columns from levels
+#' mt_update_factors(df, meta_tbl, overwrite = F)
+#' # update metadata for columns indicated as factors in the metadata table
+#' mt_update_factors(df, meta_tbl, factor_cols = get_factors_meta(meta_tbl), overwrite = F)
+#' }
+mt_update_factors <- function(df, meta_tbl, factor_cols = NULL, sep = ";",
+                              overwrite = F){
+
+    if(is.null(factor_cols)){
+        factor_cols <- get_factors_df(df)}
+
+    if(!all(factor_cols %in% names(df))){
+        stop(factor_cols[which(!factor_cols %in% names(df))],
+             " not a valid colname")
+    }
+    for(col in factor_cols){
+        empty <- check_empty(col, meta_tbl, c("code", "levels"))
+        if(empty | overwrite){
+            meta_tbl[meta_tbl$attributeName == col, c("code", "levels")] <-
+                collapse_factor_levels(df[,col, drop = T], sep)
+            meta_tbl[meta_tbl$attributeName == col, "columnClasses"] <-
+                "factor"
+        }else{
+            stop("Action would overwrite current factor metadata,
+                      use overwrite = T to override this")}
+    }
+
+    done(field("meta_tbl"), " fields ", value("code"), " & ", value("levels"),
+         " successfully updated for vars: ",
+         value(paste0(factor_cols, collapse = ", "))," \n")
+
+    meta_tbl
 }
 
 #' Extract attribute table
@@ -120,7 +156,7 @@ mt_extract_attr_tbl <- function(meta_tbl) {
 mt_extract_factors_tbl <- function(meta_tbl, sep =";") {
     meta_tbl[] <- lapply(meta_tbl, as.character)
     factors <- NULL
-    vars <- meta_tbl$attributeName[meta_tbl$columnClasses %in% c("ordered", "factor")]
+    vars <- mt_extract_factor_cols(meta_tbl)
     for(var in vars){
         x <- meta_tbl[meta_tbl$attributeName == var, , drop = F]
         f_tbl <- data.frame(
@@ -140,5 +176,73 @@ collapse_factor_levels <- function(x, sep = ";"){
     }
 }
 
+# extract a vector of column names containing `factor` or `ordered` data.
+mt_extract_factor_cols <- function(meta_tbl){
+    vars <- meta_tbl$attributeName[meta_tbl$columnClasses %in%
+                                       c("ordered", "factor")]
+}
 
+# check_empty
+check_empty <- function(data_col, meta_tbl,
+                        meta_fields){
 
+    match.arg(arg = data_col, choices = get_vars_meta(meta_tbl))
+    match.arg(arg = meta_fields, choices = input_meta_fields(meta_tbl),
+              several.ok = T)
+
+    non_empty_meta_fields_test <- is.na(meta_tbl[meta_tbl$attributeName == data_col,
+                                                 meta_fields] %>% unlist)
+    if(all(non_empty_meta_fields_test)){
+        return(TRUE)
+    }else{
+        non_empty_meta_fields <-  meta_fields[!non_empty_meta_fields_test]
+        todo(field("data_col "), value(data_col), ": ", field("meta_tbl "),
+             "already contain metadata for ", field("meta_field "),
+             value(non_empty_meta_fields), " \n")
+        return(FALSE)
+    }
+}
+
+input_meta_fields <- function(meta_tbl){
+    names(meta_tbl) %>% magrittr::extract(! . %in% c("attributeName",
+                                                     "columnClasses"))
+}
+
+#' Extract useful metadata from data and metadata tables
+#'
+#' @param df a dataframe of data for which metadata attribute table is to be created
+#' @param meta_tbl a metadata table associated with df
+#'
+#' @return character vectors of variables names
+#' @details functions appended `df` extract information from a `df` of the data,
+#'  while those appended `meta` from the `meta_tbl` containing the attribute metadata.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' get_vars_df(df)
+#' get_vars_meta(meta_tbl)
+#' get_factors_df(df)
+#' get_factors_meta(meta_tbl)
+#' }
+get_vars_df <- function(df){
+    names(df)
+}
+
+#' @inherit get_vars_df
+#' @export
+get_vars_meta <- function(meta_tbl){
+    meta_tbl$attributeName
+}
+
+#' @inherit get_vars_df
+#' @export
+get_factors_meta <- function(meta_tbl){
+    meta_tbl$attributeName[meta_tbl$columnClasses == "factor"]
+}
+
+#' @inherit get_vars_df
+#' @export
+get_factors_df <- function(df){
+    names(df)[sapply(df, FUN = is.factor)]
+}
